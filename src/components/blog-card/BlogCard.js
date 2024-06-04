a504792fbcd3815060a7249ca280aeb7a9538ff7
natalieysmith <141788604+natalieysmith@users.noreply.github.com>
2024-06-04 14:21:14 +1000

    Nat - clean up carousel

--- src/components/blog-card/BlogCard.js
+++ src/components/blog-card/BlogCard.js
@@ -1,36 +1,28 @@
 import React from "react";
 import { useState } from "react";
-import Card from "react-bootstrap/Card";
-import Tilt from "react-parallax-tilt";
 import "./BlogCard.css";
-import PlusLogo from "./../../assets/icons/PlusIcon.svg";
 
 const BlogCard = (props) => {
-  const [showCard, setShowCard] = useState(false);
-
-  const title = props.title;
-  const content = props.content;
-  const link = props.link;
-  const image = props.image;
-  const colour = props.colour;
-  const alt = props.alt;
-
   return (
-    <div className={showCard ? "card-resize" : "card-normal"}>
+    <div className="card-normal">
       <div className="card blog-card">
-        {/* <Tilt > */}
         <div>
           <div>
             <div className="card-image">
-              <img className="" src={image} alt={alt} />
+              <img
+                className=""
+                src={require("../../assets/img/carousal/" + props.image)}
+                alt={props.alt}
+              />
             </div>
             <p className="card-title">{props.title}</p>
-            <p>{props.content}</p>
-            <button src={link}>Find out more</button>
+            <div className="card-content">
+              <p>{props.content}</p>
+              <button src={props.link}>Find out more</button>
+            </div>
           </div>
         </div>
       </div>
-      {/* </Tilt> */}
     </div>
   );
 };
